a1aab0af258f5352968dd84ded2fdb409b7d4486
= <danielcf.vix@hotmail.com>
2023-02-07 13:42:15 -0300

    fix

--- main.py
+++ main.py
@@ -3,18 +3,23 @@ from management import product_handler, tab_handler
 if __name__ == "__main__":
     # Seus prints de teste aqui
     print(product_handler.get_product_by_id(25))
+
     print(product_handler.get_products_by_type("drink"))
+
     test = {
             "title": "Bolinho JS",
             "price": 1.0,
             "rating": 2,
             "description": "Bolinho de JS com cenoura",
             "type": "bakery"}
+
     print(product_handler.add_product(**test))
+
     print(tab_handler.calculate_tab([
                     {"_id": 10, "amount": 3},
                     {"_id": 20, "amount": 2},
                     {"_id": 21, "amount": 5},
                 ]))
+                
     print(product_handler.menu_report())
     ...
--- management/product_handler.py
+++ management/product_handler.py
@@ -4,8 +4,8 @@ from menu import products
 def get_product_by_id(id: int):
     if not type(id) == int:
         raise TypeError("product id must be an int")   
-    for element in products:
 
+    for element in products:
         if element["_id"] == id:
             return element
 
@@ -41,10 +41,12 @@ def menu_report():
     count_product = 0
     average_price = 0
     most_common_type = []
+
     for item in products:
             count_product +=1
             average_price += item['price']
             most_common_type.append(item['type'])
+
     most_common_type_aux = mode(most_common_type)
     average_price_aux = round(average_price/count_product, 2)
     return f"Products Count: {count_product} - Average Price: ${average_price_aux} - Most Common Type: {most_common_type_aux}"
